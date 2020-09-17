7a9c111d088b6914976acbd548256200c3f70ca4
greta19 <greta19@ru.is>
2020-09-17 15:26:17 +0000

    4th commit

--- TileTravelers.py
+++ TileTravelers.py
@@ -1,39 +1,64 @@
 
 def travel(x,y):
+    n=s=w=e=0
     if x==1:
         if y==1:
             place = '(N)orth'
+            n=1
         elif y==2:
             place = '(N)orth or (E)ast or (S)outh'
+            n=e=s=1
         elif: y == 3:
             place = '(E)ast or (S)outh'
+            e=s=1
     if x == 2:
          if y==1:
             place = '(N)orth'
+            n=1
         elif y==2:
             place = '(W)est or (S)outh'
+            w=s=1
         elif: y == 3:
             place = '(E)ast or (W)est'
+            e=w=1
     if x == 3:
          if y==1:
             V = 1
         elif y==2:
             place = '(N)orth or (S)outh'
+            n=s=1
         elif: y == 3:
             place = '(W)est or (S)outh'
+            w=s=1
 
 def direction(x,y,k):
     if k == 'N' or k == 'n':
-        return x , y +1
+        if n==0:
+            ans="Not a valid direction!"
+            return ans
+        else:
+            return x , y +1
 
     elif k == 'E' or k == 'e':
-        return x+1 , y
+        if e==0:
+            ans="Not a valid direction!"
+            return ans
+        else:
+            return x+1 , y
 
     elif k == 'S' or k== 's':
-        return x , y-1
+        if s==0:
+            ans="Not a valid direction!"
+            return ans
+        else:
+            return x , y-1
     
     elif k == 'W' or k == 'w':
-        return x-1 , y
+        if w==0:
+            ans="Not a valid direction!"
+            return ans
+        else:
+            return x-1 , y
 
 
 x = 1
