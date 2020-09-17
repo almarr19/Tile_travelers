672e787a374ca73c3d2cd82a2f4a6e48c9074945
aslaugsol <70697649+aslaugsol@users.noreply.github.com>
2020-09-17 15:00:12 +0000

    :)

--- TileTravelers.py
+++ TileTravelers.py
@@ -1,10 +1,40 @@
 
-x=9
-y=8
 def travel(x,y):
-    if x==2:
-        if y==3:
-            print("")
+    if x==1:
+        if y==1:
+            place = '(N)orth'
+        elif y==2:
+            place = '(N)orth or (E)ast or (S)outh'
+        elif: y == 3:
+            place = '(E)ast or (S)outh'
+    if x == 2:
+         if y==1:
+            place = '(N)orth'
+        elif y==2:
+            place = '(W)est or (S)outh'
+        elif: y == 3:
+            place = '(E)ast or (W)est'
+    if x == 3:
+         if y==1:
+            V = 1
+        elif y==2:
+            place = '(N)orth or (S)outh'
+        elif: y == 3:
+            place = '(W)est or (S)outh'
+
+def direction(k,y,x):
+    if k == 'N' or k == 'n':
+        if 
+        y = y + 1
+
+x = 1
+y = 1
+
+while V!= 1:
+    travel(x,y)
+    k = input('Direction: ')
+
+    
 
 
 
